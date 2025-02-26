80c94130b058e1f666127ee38b9455f70ff6eff3
rish7087 <rishabhsaini40618@gmail.com>
2025-02-26 13:19:46 +0530

    modified:   server/app.js 	modified:   server/package-lock.json

--- server/app.js
+++ server/app.js
@@ -37,8 +37,11 @@ app.use(cors({
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+console.log("Mongo URI from env:", process.env.MONGO_URI);
+
 // Session middleware
 app.use(session({
+    
   secret: process.env.SESSION_SECRET || 'your_secret_key', // Use environment variable for secret
   resave: false,
   saveUninitialized: true,
